789308931bf953184881a009a1a1c3908f3753ac
NNeto <no.neto@live.com>
2018-01-26 15:23:21 -0300

    Fix icons and CSS selectors for the login page
    Fix selectors to match the correct ids in the layout and fix some rules to address the new SVG + JS Font Awesome icons

--- App/View/js/login-page-form.js
+++ App/View/js/login-page-form.js
@@ -7,18 +7,18 @@ for ( li of abas ) {
         if ( this.id == "login_tab" && this.className.indexOf("selected") == -1 ) {
 
             document.getElementById("login_tab").className += "selected";
-            document.getElementById("login-form-wrapper").className += "selected";
+            document.getElementById("login-form").className += "selected";
 
             document.getElementById("register_tab").className = document.getElementById("register_tab").className.replace(/selected/g, "");
-            document.getElementById("register-form-wrapper").className = document.getElementById("register-form-wrapper").className.replace(/selected/g, "");
+            document.getElementById("register-form").className = document.getElementById("register-form").className.replace(/selected/g, "");
 
         } else if ( this.id == "register_tab" && this.className.indexOf("selected") == -1 ) {
 
             document.getElementById("register_tab").className += "selected";
-            document.getElementById("register-form-wrapper").className += "selected";
+            document.getElementById("register-form").className += "selected";
 
             document.getElementById("login_tab").className = document.getElementById("login_tab").className.replace(/selected/g, "");
-            document.getElementById("login-form-wrapper").className = document.getElementById("login-form-wrapper").className.replace(/selected/g, "");
+            document.getElementById("login-form").className = document.getElementById("login-form").className.replace(/selected/g, "");
         }
     });
 }
