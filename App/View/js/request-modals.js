d6fc953920e7dd0b5e8be5b549d4fdf08a849274
Noberto Pessoa <no.neto@live.com>
2018-03-08 18:59:08 -0300

    Combine modals into one and add it in the remaining pages
    - Modals for open and pending requests had the same ids, preventing one page to be able to show both kinds. Combining them also minimizes duplicate code;
    - Add code to open modals on the admin page;
    - Adapt the modals code on pages in which they were already included;

--- App/View/js/request-modals.js
+++ App/View/js/request-modals.js
@@ -16,17 +16,19 @@ function fillOpenRequestModal (request_id) {
         {"request_id":request_id},
         function(data, status) {
             var request = JSON.parse(data);
-            $('#detalhes-chamado-modal-form')[0].elements["id_solicitacao_field"].value = request.id_solicitacao ? request.id_solicitacao : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["status_field"].value = request.chamado_status ? request.chamado_status : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["servico_field"].value = request.id_servico ? request.id_servico : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["data_abertura_field"].value = request.data_abertura ? request.data_abertura : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["data_finalizado_field"].value = request.data_finalizado ? request.data_finalizado : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["prazo_field"].value = request.prazo ? request.prazo : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["tecnico_responsavel_field"].value = request.id_tecnico_responsavel ? request.id_tecnico_responsavel : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["tecnico_abertura_field"].value = request.id_tecnico_abertura ? request.id_tecnico_abertura : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["descricao_field"].value = request.descricao ? request.descricao : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["parecer_tecnico_field"].value = request.parecer_tecnico ? request.parecer_tecnico : "Indefinido";
-            $('#detalhes_chamado').modal('toggle');
+            $('.open-request-modal-form').css("display", "block");
+            $('.call-request-modal-form').css("display", "none");
+            $('.open-request-modal-form')[0].elements["id_solicitacao_field"].value = request.id_solicitacao ? request.id_solicitacao : "Indefinido";
+            $('.open-request-modal-form')[0].elements["status_field"].value = request.chamado_status ? request.chamado_status : "Indefinido";
+            $('.open-request-modal-form')[0].elements["servico_field"].value = request.id_servico ? request.id_servico : "Indefinido";
+            $('.open-request-modal-form')[0].elements["data_abertura_field"].value = request.data_abertura ? request.data_abertura : "Indefinido";
+            $('.open-request-modal-form')[0].elements["data_finalizado_field"].value = request.data_finalizado ? request.data_finalizado : "Indefinido";
+            $('.open-request-modal-form')[0].elements["prazo_field"].value = request.prazo ? request.prazo : "Indefinido";
+            $('.open-request-modal-form')[0].elements["tecnico_responsavel_field"].value = request.id_tecnico_responsavel ? request.id_tecnico_responsavel : "Indefinido";
+            $('.open-request-modal-form')[0].elements["tecnico_abertura_field"].value = request.id_tecnico_abertura ? request.id_tecnico_abertura : "Indefinido";
+            $('.open-request-modal-form')[0].elements["descricao_field"].value = request.descricao ? request.descricao : "Indefinido";
+            $('.open-request-modal-form')[0].elements["parecer_tecnico_field"].value = request.parecer_tecnico ? request.parecer_tecnico : "Indefinido";
+            $('.request-modal').modal('toggle');
         });
 }
 
@@ -36,11 +38,13 @@ function fillRequestCallModal (request_id) {
         {"request_id":request_id},
         function(data, status) {
             var request = JSON.parse(data);
-            $('#detalhes-chamado-modal-form')[0].elements["id_solicitacao_field"].value = request.id_solicitacao ? request.id_solicitacao : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["status_field"].value = request.solicitacao_chamado_status ? request.solicitacao_chamado_status : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["servico_field"].value = request.id_servico ? request.id_servico : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["data_solicitacao_field"].value = request.data_solicitacao ? request.data_solicitacao : "Indefinido";
-            $('#detalhes-chamado-modal-form')[0].elements["descricao_field"].value = request.descricao ? request.descricao : "Indefinido";
-            $('#detalhes_chamado').modal('toggle');
+            $('.open-request-modal-form').css("display", "none");
+            $('.call-request-modal-form').css("display", "block");
+            $('.call-request-modal-form')[0].elements["id_solicitacao_field"].value = request.id_solicitacao ? request.id_solicitacao : "Indefinido";
+            $('.call-request-modal-form')[0].elements["status_field"].value = request.solicitacao_chamado_status ? request.solicitacao_chamado_status : "Indefinido";
+            $('.call-request-modal-form')[0].elements["servico_field"].value = request.id_servico ? request.id_servico : "Indefinido";
+            $('.call-request-modal-form')[0].elements["data_solicitacao_field"].value = request.data_solicitacao ? request.data_solicitacao : "Indefinido";
+            $('.call-request-modal-form')[0].elements["descricao_field"].value = request.descricao ? request.descricao : "Indefinido";
+            $('.request-modal').modal('toggle');
         });
 }
