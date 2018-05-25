ad04c8b7a92b2d7c75798c868676e627b84fe003
Noberto Pessoa <no.neto@live.com>
2018-05-25 17:38:29 -0300

    Add lastname field in register form and other improvements
    Make related changes in CSS and JS files to accommodate the changes made
    to include the field in the form and the changes to make the form be
    submitted with JS.
    Also:
    - remove residual echo's;
    - add email address validation;
    - organize code by adding some comments and moving JS code from .phtml
    to the corresponding .js;

--- App/Controller/IndexController.php
+++ App/Controller/IndexController.php
@@ -25,60 +25,61 @@ class IndexController extends Action{
 
   public function solicitar_acesso(){
     $nome = $_POST['nomeCliente'];
+    $sobrenome = $_POST['sobrenomeCliente'];
     $email = $_POST['emailCliente'];
     $login = $_POST['loginCliente'];
     $setor = $_POST['setorCliente'];
     $matricula = $_POST['matriculaCliente'];
 
-    $acesso = Container::getClass("SolicitarAcesso");
-    $acesso->save($nome,$email,$login,$setor,$matricula);
-    echo "<script>alert('Solicitação recebida.\\nAguarde entrarmos em contato através do email'); history.back();</script>";
+    // Validate email address
+    if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $acesso = Container::getClass("SolicitarAcesso");
+      $acesso->save($nome." ".$sobrenome,$email,$login,$setor,$matricula);
+    } else {
+      header('Content-Type: application/json; charset=UTF-8');
+      header('HTTP/1.1 400');
+      echo json_encode(array('event' => 'error', 'type' => 'invalid_email'));
+    }
   }
 
   public function logar(){
-    if(isset($_POST['login_accept'])){
-      if(($_POST['username'] != '') && ($_POST['password'] != '')){
-        $login = $_POST['username'];
-        $pass = $_POST['password'];
+    if(($_POST['username'] != '') && ($_POST['password'] != '')){
+      $login = $_POST['username'];
+      $pass = $_POST['password'];
 
-        $acesso = Container::getClass("Usuario");
-        $user = $acesso->findByLogin($login);
-        if($user['password'] == $pass){
-            echo 'bem vindo :'.$user['login'].' - '.$user['password'];
+      $acesso = Container::getClass("Usuario");
+      $user = $acesso->findByLogin($login);
+      if($user['password'] == $pass){
+        $user_role = Container::getClass("UsuarioRole");
+        $role = $user_role->findByIdUser($user['id']);
 
-            $user_role = Container::getClass("UsuarioRole");
-            $role = $user_role->findByIdUser($user['id']);
+        session_start();
+        $_SESSION['user_id'] = $user['id'];
+        $_SESSION['user_name'] = $user['nome'];
+        $_SESSION['user_login'] = $user['login'];
+        $_SESSION['user_turno'] = $user['turno'];
+        $_SESSION['user_setor'] = $user['setor'];
+        $_SESSION['user_matricula'] = $user['matricula'];
 
-            session_start();
-            $_SESSION['user_id'] = $user['id'];
-            $_SESSION['user_name'] = $user['nome'];
-            $_SESSION['user_login'] = $user['login'];
-            $_SESSION['user_turno'] = $user['turno'];
-            $_SESSION['user_setor'] = $user['setor'];
-            $_SESSION['user_matricula'] = $user['matricula'];
-
-            if($role['cliente'] == 1){
-              $_SESSION['user_role'] = "CLIENTE";
-              header('Location: /gticchla/public/cliente');
-            }else if($role['tecnico'] == 1){
-              $_SESSION['user_role'] = "TECNICO";
-              header('Location: /gticchla/public/tecnico');
-            }else if($role['gerente'] == 1){
-              $_SESSION['user_role'] = "GERENTE";
-              header('Location: /gticchla/public/admin');
-            }
-
-            echo '</br> role: '.$_SESSION['user_role'];
-        }else{
-          echo "<script>alert('Usuário não encontrado'); history.back();</script>";
+        if($role['cliente'] == 1){
+          $_SESSION['user_role'] = "CLIENTE";
+          header('Location: /gticchla/public/cliente');
+        }else if($role['tecnico'] == 1){
+          $_SESSION['user_role'] = "TECNICO";
+          header('Location: /gticchla/public/tecnico');
+        }else if($role['gerente'] == 1){
+          $_SESSION['user_role'] = "GERENTE";
+          header('Location: /gticchla/public/admin');
         }
       }else{
-        echo "<script>alert('digite login e senha!'); history.back();</script>";
+        header('Content-Type: application/json; charset=UTF-8');
+        header('HTTP/1.1 400');
+        echo json_encode(array('event' => 'error', 'type' => 'invalid_credentials'));
       }
-
-
     }else{
-      echo 'n conseguiu logar';
+      header('Content-Type: application/json; charset=UTF-8');
+      header('HTTP/1.1 400');
+      echo json_encode(array('event' => 'error', 'type' => 'missing_data'));
     }
   }
 
--- App/View/js/login-page-form.js
+++ App/View/js/login-page-form.js
@@ -1,3 +1,5 @@
+// Code to alternate tabs, including via keyboard
+
 var alternateTab = function (event) {
     if (event.which == 1 || event.which == 32 || event.which == 13) {
         if ( this.id == "login_tab" && !$(this).hasClass("selected") ) {
@@ -29,6 +31,151 @@ $('#botoes-abas').children().each(function() {
     this.addEventListener("click", alternateTab, false);
     this.addEventListener("keydown", alternateTab, false);
 });
+
+
+
+
+// Validate input values
+function inputsAreValid(form) {
+    var all_inputs_filled = true;
+    
+    // If register form, check if name and last name only contains letters and spaces
+    if (form == "register-form") {
+        if (!$('#' + form + ' input[id=nomeCliente]').val().match(/^[a-zA-Z]*$/)
+        || !$('#' + form + ' input[id=sobrenomeCliente]').val().match(/^[a-zA-Z]*$/)) {
+            alert("Nome e sobrenome só podem conter letras e espaços!");
+            return false;
+        }
+    }
+
+    // Check if all inputs are filled in
+    $('#' + form + ' input[type=text]').each(function() {
+        if ($(this).val().match(/^\s*$/)) {
+            all_inputs_filled = false;
+            return false;
+        }
+    });
+    if (all_inputs_filled) {
+        return true;
+    } else {
+        alert("Preencha todos os campos");
+        return false;
+    }
+}
+
+// Submit login form
+function submitLoginForm() {
+    // Disable button
+    $("input[id=login-submit]").val("Aguarde...");
+    $("input[id=login-submit]").addClass("disabled");
+    $("input[id=login-submit]").prop("disabled", true);
+
+    // Validate form input values
+    if (inputsAreValid("login-form")) {
+        // Get form data
+        var form = $('form[id=login-form]').get(0);
+        var postData = {
+            "username": form['username'].value,
+            "password": form['password'].value
+        }
+        // Submit
+        $.post("/gticchla/public/logar", postData)
+        .done(function(data) {
+            // Success
+            window.location.reload(true);
+        }).fail(function(data) {
+            // Failure
+            var response = data.responseJSON;
+            if (response.event == "error") {
+                if (response.type == "invalid_credentials") {
+                    alert("O usuário não foi encontrado");
+                } else {
+                    alert("Não foi possível no momento");
+                }
+                $("input[id=login-submit]").val("Acessar");
+                $("input[id=login-submit]").removeClass("disabled");
+                $("input[id=login-submit]").prop("disabled", false);
+            }
+        });
+    } else {
+        $("input[id=login-submit]").val("Acessar");
+        $("input[id=login-submit]").removeClass("disabled");
+        $("input[id=login-submit]").prop("disabled", false);
+    }
+}
+
+// Login form event listeners
+$('input[id=login-submit]').on('click', function(e) {
+    submitLoginForm();
+});
+$("input[id=username], input[id=password]").on('keyup', function(e) {
+    if (e.which == 13) {
+        submitLoginForm();
+    }
+});
+
+
+
+
+// Submit request access form
+function submitRequestAccessForm() {
+    // Disable button
+    $("input[id=register-submit]").val("Aguarde...");
+    $("input[id=register-submit]").addClass("disabled");
+    $("input[id=register-submit]").prop("disabled", true);
+
+    // Validate form input values
+    if(inputsAreValid("register-form")) {
+        // Get form data
+        var form = $('form[id=register-form]').get(0);
+        var postData = {
+            "nomeCliente": form['nomeCliente'].value,
+            "sobrenomeCliente": form['sobrenomeCliente'].value,
+            "emailCliente": form['emailCliente'].value,
+            "loginCliente": form['loginCliente'].value,
+            "setorCliente": form['setorCliente'].value,
+            "matriculaCliente": form['matriculaCliente'].value
+        }
+
+        // Submit
+        $.post("/gticchla/public/solicitar_acesso", postData)
+        .done(function(data) {
+            // Success
+            alert("Solicitação recebida!\nCheque o e-mail fornecido para verificar os dados.");
+            window.location.reload(true);
+        }).fail(function(data) {
+            // Failure
+            var response = data.responseJSON;
+            if (response.event == "error") {
+                if (response.type == "invalid_email") {
+                    alert("O endereço de email inserido é inválido");
+                } else {
+                    alert("Não foi possível no momento");
+                }
+                $("input[id=register-submit]").val("Solicitar acesso");
+                $("input[id=register-submit]").removeClass("disabled");
+                $("input[id=register-submit]").prop("disabled", false);
+            }
+        });
+    } else {
+        $("input[id=register-submit]").val("Solicitar acesso");
+        $("input[id=register-submit]").removeClass("disabled");
+        $("input[id=register-submit]").prop("disabled", false);
+    }
+}
+
+// Request access form event listeners
+$('input[id=register-submit]').on('click', function(e) {
+    submitRequestAccessForm();
+});
+$("form[id=register-form] input[type=text]").on('keyup', function(e) {
+    if (e.which == 13) {
+        submitRequestAccessForm();
+    }
+});
+
+
+
 
 $(document).ready(function() {
     $('input#username').focus();
