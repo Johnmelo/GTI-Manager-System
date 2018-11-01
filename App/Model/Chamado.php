b123ce3dea0f038baef1cd4ac574b5cf835f0ef9
Noberto Pessoa <no.neto@live.com>
2018-11-01 13:49:14 -0300

    Add function in Chamado model to get a ticket data
    Also, return the new ticket id when created in the DB.

--- App/Model/Chamado.php
+++ App/Model/Chamado.php
@@ -4,6 +4,28 @@ use SON\Db\Table;
 class Chamado extends Table{
   protected $table = "chamados";
 
+  public function getTicketById($ticketId) {
+    $stmt = $this->db->prepare(
+      "SELECT `c`.`id` AS `id_chamado`, `sc`.`id` AS `id_solicitacao`, `u1`.`nome` AS `cliente`, ".
+      "`c`.`id_tecnico_responsavel`, `l`.`nome` AS `local`, `s`.`nome` AS `servico`, `c`.`descricao`, ".
+      "`c`.`status`, `sc`.`data_solicitacao`, `c`.`data_abertura`, `c`.`data_finalizado`, `c`.`prazo`, ".
+      "`u2`.`nome` AS `tecnico_abertura`, `u3`.`nome` AS `tecnico_responsavel`, `c`.`parecer_tecnico` ".
+      "FROM `{$this->table}` AS `c` ".
+      "LEFT JOIN `solicitacao_chamado` AS `sc` ON `sc`.`id` = `c`.`id_solicitacao` ".
+      "LEFT JOIN `servicos` AS `s` ON `s`.`id` = `sc`.`id_servico` ".
+      "LEFT JOIN `usuarios` AS `u1` ON `u1`.`id` = `c`.`id_cliente_solicitante` ".
+      "LEFT JOIN `usuarios` AS `u2` ON `u2`.`id` = `c`.`id_tecnico_abertura` ".
+      "LEFT JOIN `usuarios` AS `u3` ON `u3`.`id` = `c`.`id_tecnico_responsavel` ".
+      "LEFT JOIN `locais` AS `l` ON `l`.`id` = `sc`.`id_local` ".
+      "WHERE `c`.`id` = :ticketId ".
+      "ORDER BY `c`.`data_abertura` "
+    );
+    $stmt->bindParam(":ticketId", $ticketId);
+    $stmt->execute();
+    $res = $stmt->fetch(\PDO::FETCH_ASSOC);
+    return $res;
+  }
+
   public function getChamadosByStatus($status){
     $stmt = $this->db->prepare("Select * from {$this->table} where status=:status");
     $stmt->bindParam(":status",$status);
@@ -128,7 +150,7 @@ class Chamado extends Table{
     $params = array($service_id, $place_id, $request_id, $id_open_technician, $id_client, $deadline, $description);
     $stmt = $this->db->prepare($query);
     if ($stmt->execute($params) && $stmt->rowCount() > 0) {
-      return true;
+      return $this->db->lastInsertId();
     } else {
       return false;
     }
